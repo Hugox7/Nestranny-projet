f947aedb86fbfbd4d1d12968d084277018d3d59c
Hugo Trichard <hugo.trichard@gmail.com>
2018-10-20 00:53:56 +0200

    ajout du logo et du bouton dropdown

--- src/composants/Missions.js
+++ src/composants/Missions.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './Missions.css';
 import MediaCard from './MediaCard';
+import { TrashList }  from './TrashList';
 
 const Missions = () => {
-    return(
-        <div>
-            <MediaCard 
-                
+    const card = TrashList.map((elem, index) => {
+        return (
+            <MediaCard
+                key={index}
+                title={elem.title}
+                text1={elem.text1}
+                text2={elem.text2}
+                text3={elem.text3}
+                img={elem.img}
             />
+        )
+    })
+
+    return(
+        <div className = "card-list">
+            {card}
         </div>
     )
 }
