52eb13dda8f2ae4a8db773dece72c9e3f5af4499
Hugo Trichard <hugo.trichard@gmail.com>
2018-10-24 16:05:43 +0200

    ajout tutu

---
+++ tutu/ContactForm.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import { TextField, Button } from '@material-ui/core';
+import './ContactForm.css';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+
+
+
+const adress = () => {
+  return 'hugo.trichard@yahoo.fr'
+}
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+        main: '#035913'
+    }
+  },
+  typography: { useNextVariants: true },
+});
+
+
+
+
+const ContactForm = () => {
+    return(
+      
+      <div className="contact-form">
+          <MuiThemeProvider theme={theme}>
+              <form id="form" action={`//formspree.io/${adress()}`} method="POST">
+                  <TextField
+                      id="standard-with-placeholder"
+                      label="Votre adresse mail"
+                      placeholder="Votre adresse mail..."
+                      className="text-field"
+                      margin="normal"
+                      type="email"
+                      name="_replyto"
+                  />
+                  <TextField
+                      id="outlined-multiline-static"
+                      label="Votre message"
+                      multiline
+                      rows="10"
+                      placeholder="Votre message..."
+                      className="text-field"
+                      margin="normal"
+                      variant="outlined"
+                      name="message"
+                  />
+                  <TextField
+                    className="hidden-input"
+                    type="text" 
+                    name="_gotcha" 
+                  />
+                  <TextField 
+                    className="hidden-input" 
+                    type="hidden" 
+                    name="_subject" 
+                    value="Website contact" 
+                  />
+                  <TextField 
+                    className="hidden-input" 
+                    type="hidden" 
+                    name="_next" 
+                    value="/merci"
+                  />
+                  <Button variant="outlined" className="button" type="submit" value="send">
+                    Envoyer
+                  </Button>
+              </form>
+          </MuiThemeProvider>
+        
+        </div>
+    )
+}
+
+export default ContactForm;
+
+/*import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import purple from '@material-ui/core/colors/purple';
+
+
+const styles = theme => ({
+  cssLabel: {
+    '&$cssFocused': {
+      color: purple[500],
+    },
+  },
+  
+  cssUnderline: {
+    '&:after': {
+      borderBottomColor: purple[500],
+    },
+  },
+});
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+        main: '#035913'
+    }
+  },
+  typography: { useNextVariants: true },
+});
+
+function CustomizedInputs(props) {
+  const { classes } = props;
+
+  return (
+    <div>
+      <MuiThemeProvider theme={theme}>
+        <TextField
+          className="text-field"
+          label="MuiThemeProvider"
+          id="mui-theme-provider-input"
+        />
+    </MuiThemeProvider>
+    </div>
+  );
+}
+
+CustomizedInputs.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(CustomizedInputs);*/
